c0315d2c8b9dfb3f0d8c850b0505c0093814bebc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-13 01:24:27 +0700

    first method for scedulling

--- src/main/java/com/irdaislakhuafa/tasksceduler/scedullers/FirstTaskSceduller.java
+++ src/main/java/com/irdaislakhuafa/tasksceduler/scedullers/FirstTaskSceduller.java
@@ -1,5 +1,25 @@
 package com.irdaislakhuafa.tasksceduler.scedullers;
 
+import java.text.SimpleDateFormat;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAsync
 public class FirstTaskSceduller {
+    /*
+     * peraturan
+     * 1. method wajib void/tidak memiliki return
+     * 2. method tidak boleh memiliki parameter
+     */
+
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("DD MMMM YYYY | HH:mm:ss");
+
+    @Scheduled(fixedDelay = (1 * 1000))
+    public void myName() {
+        System.out.println("Irda Islakhu Afa => " + dateFormatter.format(System.currentTimeMillis()));
+    }
 
 }
