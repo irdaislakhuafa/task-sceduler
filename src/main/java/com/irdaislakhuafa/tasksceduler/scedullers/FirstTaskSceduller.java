8dc60bdefb97863cbf58eebf39f66988b5520a2b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-13 01:41:29 +0700

    use initialDelay, fixedDelay and fixedRate

--- src/main/java/com/irdaislakhuafa/tasksceduler/scedullers/FirstTaskSceduller.java
+++ src/main/java/com/irdaislakhuafa/tasksceduler/scedullers/FirstTaskSceduller.java
@@ -2,6 +2,7 @@ package com.irdaislakhuafa.tasksceduler.scedullers;
 
 import java.text.SimpleDateFormat;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,9 +18,27 @@ public class FirstTaskSceduller {
 
     private static SimpleDateFormat dateFormatter = new SimpleDateFormat("DD MMMM YYYY | HH:mm:ss");
 
+    // will waiting current thread finish before next thread will be execute
     @Scheduled(fixedDelay = (1 * 1000))
+    @Async
     public void myName() {
         System.out.println("Irda Islakhu Afa => " + dateFormatter.format(System.currentTimeMillis()));
+    }
+
+    // without waiting current thread to execute next thread
+    @Scheduled(fixedRate = 1000)
+    @Async
+    public void sheName() throws Exception {
+        Thread.sleep(10000);
+        System.out.println("\n(" + Thread.currentThread().getName() + ")" + "Ana Ardani => "
+                + dateFormatter.format(System.currentTimeMillis()));
+
+    }
+
+    @Scheduled(initialDelay = (10 * 1000), fixedDelay = (20 * 1000))
+    @Async
+    public void thisIsInitialMethod() {
+        System.out.println("\033\143");
     }
 
 }
